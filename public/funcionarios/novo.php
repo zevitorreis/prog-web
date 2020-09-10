8c3a2d0a677ce555006f9da38eefad9468453d20
zevitorreis <josevitorreis@gmail.com>
2020-09-09 21:45:08 -0400

    correcao

--- public/funcionarios/novo.php
+++ public/funcionarios/novo.php
@@ -13,31 +13,34 @@ ob_start();
 
               <form action="salvar.php" class="card p-2 my-4" method="POST">
               
-                  <div class="input-group">
-                      <input type="text" placeholder="Nome do Funcionario" 
-                          class="form-control" name="nome" required>
-                  </div>
-                  <div class="input-group">
-                      <input type="text" placeholder="CPF" 
-                          class="form-control" name="cpf" required>
-                  </div>
-                  <div class="input-group">
-                      <input type="text" placeholder="Telefone" 
-                          class="form-control" name="telefone" required>
-                  </div>
-                  <div class="input-group">
-                      <input type="text" placeholder="Email" 
-                          class="form-control" name="email" required>
-                  </div>
-                  <div class="input-group">
-                      <input type="text" placeholder="Endereco" 
-                          class="form-control" name="endereco" required>
-                  </div>
-                      <div class="input-group-append">
-                          <button type="submit" class="btn btn-primary">
-                              Salvar
-                          </button>
-                      </div>
+              <div class="form-group">
+                        <label for="nome">Nome do Funcionario</label>
+                        <input type="text" class="form-control" id="nome"
+                            name="nome" placeholder="Funcionario" required>
+                    </div>
+
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="cpf">CPF</label>
+                            <input type="text" class="form-control"id="preco" 
+                                name="cpf" placeholder="CPF" required>
+                        </div>                            
+                        <div class="form-group col-md-6">
+                            <label for="telefone">Telefone</label>
+                            <input type="text" class="form-control" id="telefone" 
+                                name="telefone" placeholder="Telefone" required>
+                        </div>
+						<div class="form-group col-md-6">
+                            <label for="email">E-mail</label>
+                            <input type="text" class="form-control" id="email" 
+                                name="email" placeholder="Email" required>
+                        </div>
+						<div class="form-group col-md-6">
+                            <label for="endereco">Endereco</label>
+                            <input type="text" class="form-control" id="endereco" 
+                                name="endereco" placeholder="Endereco" required>
+                        </div>
+                    </div>
                   
               </form>
               <a href="index.php" class="btn btn-secondary ml-1" role="button" aria-pressed="true">Cancelar</a>
